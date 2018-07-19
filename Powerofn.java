2232dde67280148326f0035a055427cf4d06cdfa
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-19 18:37:29 +0530

    Add files via upload

---
+++ Powerofn.java
@@ -0,0 +1,13 @@
+package guvi;
+import java.util.*;
+public class Powerofn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+int pow=sc.nextInt();
+System.out.println(""+(int)Math.pow(num, pow));
+	}
+
+}
