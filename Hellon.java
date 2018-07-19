add9b9a69727fdf53592eba02e33659ca5f1ef7a
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-19 17:38:05 +0530

    Add files via upload

---
+++ Hellon.java
@@ -0,0 +1,17 @@
+package guvi;
+import java.util.*;
+public class Hellon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+String str="Hello";
+int n=sc.nextInt();
+for(int i=0;i<n;i++)
+{
+	System.out.println(str);
+}
+sc.close();
+	}
+
+}
