84e35d933adb906dd14300d9a490f48447fb71ca
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-19 16:21:48 +0530

    Add files via upload

---
+++ Evenornot.java
@@ -0,0 +1,21 @@
+package guvi;
+import java.util.*;
+
+public class Evenornot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+if(num%2==0)
+{
+	System.out.println("Even");
+}
+else
+{
+	System.out.println("Invalid");
+}
+sc.close();
+	}
+
+}
