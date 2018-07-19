3d56f8199293c7c9f21570368f5b7532ca7e356f
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-19 17:48:43 +0530

    Add files via upload

---
+++ Sumofn.java
@@ -0,0 +1,21 @@
+package guvi;
+import java.util.*;
+public class Sumofn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int sum=0;
+while(n>0) {
+sum+=n;
+n--;
+	}
+if(sum<9)
+System.out.println(sum);
+else
+	System.out.println(sum);
+sc.close();
+	}
+
+}
