3033f1b09aba69159a129f74d99899d912d4c901
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-19 17:31:36 +0530

    Add files via upload

---
+++ Leapornot.java
@@ -0,0 +1,20 @@
+package guvi;
+import java.util.*;
+public class Leapornot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+if(num%4==0)
+{
+	System.out.println("Yes");
+}
+else
+{
+	System.out.println("No");
+}
+sc.close();
+	}
+
+}
