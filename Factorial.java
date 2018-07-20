d451680b305c4ccf751d5709f73612292fdf8103
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-20 16:54:21 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+package guvi;
+import java.util.*;
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+int fact=1;
+for(int i=1;i<=num;i++)
+{
+	fact*=i;
+}
+System.out.println(fact);
+sc.close();
+	}
+
+}
