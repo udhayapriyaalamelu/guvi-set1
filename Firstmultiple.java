c0f4f68823f299adc9878876cd40f8ad285a272b
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-20 17:04:45 +0530

    Add files via upload

---
+++ Firstmultiple.java
@@ -0,0 +1,16 @@
+package guvi;
+import java.util.*;
+public class Firstmultiple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+for(int i=1;i<=5;i++)
+{
+	System.out.print(" "+i*num);
+}
+sc.close();
+	}
+
+}
