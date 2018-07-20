f0218667c566fbb7cc7cf4fecaa40ee13f48433a
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-20 13:43:03 +0530

    Add files via upload

---
+++ Oddtwonumber.java
@@ -0,0 +1,18 @@
+package guvi;
+import java.util.*;
+public class Oddtwonumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int first=sc.nextInt();
+int last=sc.nextInt();
+for(int i=first+1;i<last;i++)
+{
+	if(i%2!=0)
+		System.out.println(i);
+}
+sc.close();
+	}
+
+}
