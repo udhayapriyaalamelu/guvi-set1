d54d54e3538d594f975137f70f1d79b27ad17d92
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-21 13:45:42 +0530

    Add files via upload

---
+++ Medianelement.java
@@ -0,0 +1,23 @@
+package guvi;
+import java.util.*;
+public class Medianelement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int size=sc.nextInt();
+int arr[]=new int[size];
+for(int i=0;i<arr.length;i++){
+	arr[i]=sc.nextInt();}
+int swap;
+for(int i=0;i<arr.length-1;i++){
+	for(int j=i+1;j<arr.length;j++){
+		if(arr[i]>arr[j]){
+			swap=arr[i];
+			arr[i]=arr[j];
+			arr[j]=swap;}}}
+System.out.println(arr[arr.length/2]);
+sc.close();
+	}
+
+}
