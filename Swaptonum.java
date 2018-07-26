9487971739391e421e9e02620e98d9ad4693eeec
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-26 17:31:50 +0530

    Add files via upload

---
+++ Swaptonum.java
@@ -0,0 +1,17 @@
+package guvi;
+import java.util.*;
+public class Swaptonum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int temp;
+temp=a;
+a=b;
+b=temp;
+System.out.println(a+" "+b);
+	}
+
+}
