da5b13fccb5559ece0c9c794ca8a903c86f1766b
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-26 11:26:09 +0530

    Add files via upload

---
+++ Formsub.java
@@ -0,0 +1,15 @@
+package guvi;
+import java.util.*;
+public class Formsub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int h1=sc.nextInt();
+int m1=sc.nextInt();
+int h2=sc.nextInt();
+int m2=sc.nextInt();
+System.out.println(Math.abs(h1-h2)+" "+Math.abs(m1-m2));
+	}
+
+}
