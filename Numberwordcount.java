0b44b13deb648a6d87260292a8217b3d01e87d94
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-26 16:45:10 +0530

    Add files via upload

---
+++ Numberwordcount.java
@@ -0,0 +1,13 @@
+package guvi;
+import java.util.*;
+public class Numberwordcount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+String str=sc.nextLine();
+String s1[]=str.split(" ");
+System.out.println(s1.length);
+	}
+
+}
