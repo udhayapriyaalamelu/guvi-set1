56fd34fe77a42aa7e255bf1b0ca18cd77630bbca
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-26 11:16:59 +0530

    Add files via upload

---
+++ Hourmin.java
@@ -0,0 +1,14 @@
+package guvi;
+import java.util.*;
+public class Hourmin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int time=sc.nextInt();
+int hour=0,min=0;
+System.out.println(time/60);
+System.out.println(time%60);
+	}
+
+}
